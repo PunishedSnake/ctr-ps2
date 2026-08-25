0d4d5fcd5dd15b77450aef4d0b5fc7bb8546b218
Hifu <artix1995@gmail.com>
2026-08-25 16:04:22 +0200

    ps2: add packed geometry benchmark interface

---
+++ ps2/geometry_bench.h
@@ -0,0 +1,8 @@
+#ifndef CTR_PS2_GEOMETRY_BENCH_H
+#define CTR_PS2_GEOMETRY_BENCH_H
+
+int CTRPS2_GeometryBenchInit(void);
+void CTRPS2_GeometryBenchSubmit(void);
+void CTRPS2_GeometryBenchWait(void);
+
+#endif
