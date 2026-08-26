52d55983f64da3ab8b6c2cd23cf193cfb3908e80
Hifu <artix1995@gmail.com>
2026-08-26 16:39:18 +0200

    PS2 native: add p2trk fixture interface

---
+++ ps2/native_track_fixture.h
@@ -0,0 +1,9 @@
+#ifndef CTR_PS2_NATIVE_TRACK_FIXTURE_H
+#define CTR_PS2_NATIVE_TRACK_FIXTURE_H
+
+#include <tamtypes.h>
+
+const void *CTRPS2_NativeTrackFixtureData(void);
+u32 CTRPS2_NativeTrackFixtureBytes(void);
+
+#endif
