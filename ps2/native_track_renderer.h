3b09eb450bdfe99445d6949848ae1ac5a7afb677
Hifu <artix1995@gmail.com>
2026-08-26 20:02:28 +0200

    ps2: split native track submit from pass retirement

--- ps2/native_track_renderer.h
+++ ps2/native_track_renderer.h
@@ -4,6 +4,12 @@
 #include <tamtypes.h>
 
 int CTRPS2_NativeTrackRendererInit(const void *track_data, u32 track_bytes);
+
+/* Submit/wait are split so frame scheduling can do unrelated EE work in between. */
+int CTRPS2_NativeTrackRendererSubmit(void);
+void CTRPS2_NativeTrackRendererWait(void);
+
+/* Convenience correctness wrapper used by the current static prototype. */
 int CTRPS2_NativeTrackRendererDraw(void);
 
 #endif
